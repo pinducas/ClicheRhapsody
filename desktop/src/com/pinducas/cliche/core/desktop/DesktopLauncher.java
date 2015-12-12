407222c49c44bcc805c240f01614aab1456df160
Paulo <paulogaspar404@hotmail.com>
2015-12-12 17:18:15 -0200

    First commit, added all the files from the libgdx basic project.

---
+++ desktop/src/com/pinducas/cliche/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pinducas.cliche.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pinducas.cliche.core.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
